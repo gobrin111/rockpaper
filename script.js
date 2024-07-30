816e69432a0ba6184ddfeedf8eba667889809803
chingpangz <143447782+gobrin111@users.noreply.github.com>
2024-07-30 15:44:24 -0400

    added ui stuff to the hmtl and the functions in the script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let humanScore=0;
 let computerScore=0;
-let humanChoice;
+let humanChoice=0;
 let computerChoice;
 const arr = ["rock","paper","scissors"];
 const roundPlay = document.querySelector("#playround");
@@ -24,10 +24,13 @@ function getHumanChoice(){
   }
 }
 
+const computerpoints = document.querySelector('#computerScore');
+const humanpoints = document.querySelector('#humanScore');
+
 function playRound(){
   getComputerChoice();
   let comp = computerChoice;
-  getHumanChoice();
+  // getHumanChoice();
   let human = humanChoice;
   if (human==comp){
     console.log("Tie")
@@ -36,10 +39,12 @@ function playRound(){
   if (human==0){
     if(comp==1){
       computerScore++;
+      computerpoints.textContent = computerScore;
       console.log("computer wins")
       return
     } else {
       humanScore++
+      humanpoints.textContent = humanScore;
       console.log("human wins")
       return
     }
@@ -47,29 +52,53 @@ function playRound(){
   if(human==1){
     if(comp==0){
       humanScore++;
+      humanpoints.textContent = humanScore;
       console.log("human wins");
     } else {
       computerScore++;
+      computerpoints.textContent = computerScore;
       console.log("computer wins");
     }
   }
   if(human==2){
     if(comp==0){
       computerScore++;
+      computerpoints.textContent = computerScore;
       console.log("computer wins");
     } else {
       humanScore++;
+      humanpoints.textContent = humanScore;
       console.log("human wins");
     }
   }
 }
 
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+const result = document.querySelector('#result');
+const userChoice = document.querySelector('#userChoice');
+
 roundPlay.addEventListener("click", () => {
   // for(let i=0; i<5; i++){
     playRound();
   // }
-  console.log("Computer: "+computerScore);
-  console.log("Human: "+humanScore);
+  // console.log("Computer: "+computerScore);
+  // console.log("Human: "+humanScore);
+})
+
+rockButton.addEventListener("click", () => {
+  userChoice.textContent ='Rock';
+  humanChoice = 0;
+
+})
+paperButton.addEventListener("click", () => {
+  userChoice.textContent = 'Paper';
+  humanChoice = 1;
+})
+scissorsButton.addEventListener("click", () => {
+  userChoice.textContent = 'Scissors';
+  humanChoice = 2;
 })
 
 // getComputerChoice();
